d96d2635f67971ca379ddc7ccae24dfa2795218d
alec aivazis <alec@aivazis.com>
2016-01-31 14:16:35 -0800

    moved sqlalchemy base model to sqlalchemy module

--- helpers/sqlalchemy/__init__.py
+++ helpers/sqlalchemy/__init__.py
@@ -1,3 +1,21 @@
-from .typeDecorators import Password as PasswordType
+# third party imports
+import nautilus
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declared_attr
+# local imports
+from .typeDecorators import Password
 from .mixins import HasPassword
-from .mixins import Base as BaseModel
+
+class BaseModel(nautilus.ext.db.Model):
+    """Convenience base DB model class. Makes sure tables in MySQL are created as InnoDB.
+    This is to enforce foreign key constraints (MyISAM doesn't support constraints) outside of production. Tables are
+    also named to avoid collisions.
+    """
+
+    @declared_attr
+    def __tablename__(self):
+        return '{}_{}'.format(self.__module__.split('.')[-1], self.__name__.lower())
+
+    __abstract__ = True
+    __table_args__ = dict(mysql_charset='utf8')
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=True)
--- helpers/sqlalchemy/mixins/__init__.py
+++ helpers/sqlalchemy/mixins/__init__.py
@@ -1,2 +1,1 @@
 from .hasPassword import HasPassword
-from .base import Base
