55795252645399906a66c763ce15e1d55361585a
alec aivazis <alec@aivazis.com>
2016-02-02 14:58:06 -0800

    BaseModel now automatically registers with the admin

--- nautilus/helpers/sqlalchemy/__init__.py
+++ nautilus/helpers/sqlalchemy/__init__.py
@@ -6,12 +6,52 @@ from sqlalchemy.ext.declarative import declarative_base
 # local imports
 from .typeDecorators import Password
 from .mixins import *
-from ...ext import db
+from ...ext import db, admin
 
 JsonBase = declarative_base(cls=(JsonSerializableBase,))
 
+class Meta(type):
+    """
+        The base metaclass for the nautilus models. Currently, it's primary use is to
+        automatically register a model class with the admin after it is created.
+    """
 
-class BaseModel(db.Model, JsonBase):
+    def __new__(cls, name, bases, attributes, **kwds):
+        """ Called after the class record is created. """
+
+        # if the cls doesn't have the attribute yet, then it must be the first
+        if not hasattr(cls, 'is_base'):
+            # mark the initial class
+            cls.is_base = True
+            attributes['is_base'] = True
+        # otherwise we are a sub class
+        else:
+            attributes['is_base'] = False
+
+        # create the next class record
+        return super().__new__(cls, name, bases, attributes, **kwds)
+
+    def __init__(self, name, bases, attributes, **kwds):
+            super().__init__(name, bases, attributes, **kwds)
+
+            # self.alec_foo = name
+            return
+
+
+class MixedMeta(Meta, type(db.Model)):
+    """
+        This meta class mixes the sqlalchemy model meta class and the nautilus one.
+    """
+    def __init__(self, name, bases, attributes, **kwds):
+        super().__init__(name, bases, attributes, **kwds)
+        # if we are the sub class of a derived one
+        if not attributes['is_base']:
+            # let the user handle
+            # admin.add_model(self)
+            self.onCreation()
+
+
+class BaseModel(db.Model, JsonBase, metaclass=MixedMeta):
 
     def __init__(self, **kwargs):
         """ treat kwargs as attribute assignment """
@@ -19,6 +59,12 @@ class BaseModel(db.Model, JsonBase):
         for key, value in kwargs.items():
             # treat them like attribute assignments
             setattr(self, key, value)
+
+    @classmethod
+    def onCreation(cls):
+        # register the class with the admin interface
+        admin.add_model(cls)
+
 
     def save(self):
         # add the entry to the db session
