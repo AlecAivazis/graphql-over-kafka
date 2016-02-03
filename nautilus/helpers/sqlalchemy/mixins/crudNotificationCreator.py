7d38c46b6ec4317d8f6283a01beb714824dac71d
alec aivazis <alec@aivazis.com>
2016-02-02 21:19:03 -0800

    cleanup

--- nautilus/helpers/sqlalchemy/mixins/crudNotificationCreator.py
+++ nautilus/helpers/sqlalchemy/mixins/crudNotificationCreator.py
@@ -1,44 +1,33 @@
+# external imports
 from sqlalchemy import event
+# local imports
 from nautilus.network import dispatchAction
 
 class CRUDNotificationCreator:
 
-    nautilus_base = True
+    nautilus_base = True # required to prevent self-application on creation
+
+    @classmethod
+    def dispatch(cls, type, target):
+        dispatchAction({
+            'type': '{}_{}'.format(cls.__name__.lower(), type),
+            'payload': target.__json__(),
+        })
+
+    @classmethod
+    def addListener(cls, db_event, action_type):
+        # on event, dispatch the appropriate action
+        @event.listens_for(cls, db_event)
+        def dispatchSaveAction(mapper, connection, target):
+            """ notifies the network of the new user model """
+            cls.dispatch(action_type, target)
+
 
     @classmethod
     def onCreation(cls):
         # perform the intended behavior
         super().onCreation()
-        # add the event listeners
-        cls.addEventCreators()
 
-    @classmethod
-    def addEventCreators(cls):
-        """ Adds the event creators to the class record """
-
-        # on save, send an action with type <model>_created
-        @event.listens_for(cls, 'after_insert')
-        def dispatchSaveAction(mapper, connection, target):
-            """ notifies the network of the new user model """
-            dispatchAction({
-                'type': 'user_created',
-                'payload': target.__json__(),
-            })
-
-        # on delete, send an action with type <model>_deleted
-        @event.listens_for(cls, 'after_delete')
-        def dispatchDeleteAction(mapper, connection, target):
-            """ notifies the network of the deleted user model """
-            dispatchAction({
-                'type': 'user_deleted',
-                'payload': target.__json__(),
-            })
-
-        # on delete, send an action with type <model>_updated
-        @event.listens_for(cls, 'after_update')
-        def dispatchUPdateAction(mapper, connection, target):
-            """ notifies the network of the updated model """
-            dispatchAction({
-                'type': 'user_updated',
-                'payload': target.__json__(),
-            })
+        cls.addListener('after_insert', 'create_success')
+        cls.addListener('after_delete', 'delete_success')
+        cls.addListener('after_update', 'update_success')
