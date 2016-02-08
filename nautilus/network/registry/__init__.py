d44a82fcf475fe86b01e0c911f4a021542f69101
alec aivazis <alec@aivazis.com>
2016-02-07 17:20:47 -0800

    services now register/deregister with a random string identifier

--- nautilus/network/registry/__init__.py
+++ nautilus/network/registry/__init__.py
@@ -12,17 +12,22 @@ consulSession = consul.Consul()
 
 def register_service(service):
     ''' Add a service to the registry service. '''
+
+    # compute and save the consul identifier for the service
+    service.consul_name = "{}-{}".format(service.name, random_string(6))
+
     # the consul service entry
     consulSession.agent.service.register(
         name = service.name,
-        service_id = "{}-{}".format(service.name, random_string(6)),
+        service_id = service.consul_name,
         port = service.app.config['PORT'],
     )
 
 
 def deregister_service(service):
     ''' Remove a service from the registery. '''
-    consulSession.agent.service.deregister(service.name)
+    consulSession.agent.service.deregister(service.consul_name)
+    consulSession.agent.check.deregister(service.consul_name)
 
 
 def get_services():
@@ -50,7 +55,7 @@ def keep_alive(service):
     register_service(service)
     # add a ttl check for the service in case we die
     consulSession.agent.check.register(
-        name = service.name,
+        name = service.consul_name,
         check = Check.ttl(str(ttl) + 's'),
     )
 
@@ -61,7 +66,7 @@ def keep_alive(service):
             # sleep every 2 seconds
             time.sleep(2)
             # tell the agent that we are passing the ttl check
-            consulSession.agent.check.ttl_pass(service.name, 'Agent alive and reachable.')
+            consulSession.agent.check.ttl_pass(service.consul_name, 'Agent alive and reachable.')
 
     # create a thread that will run the consumer
     thread = threading.Thread(target=run_check)
