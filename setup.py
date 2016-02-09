8022b47754d2cc33a979a8a8797a46b194deca88
alec aivazis <alec@aivazis.com>
2016-02-08 19:49:10 -0800

    updated build files

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'nautilus',
-    packages = ['nautilus'],
-    version = '0.1.0',
+    version = '0.2.4',
     description = 'A library for creating microservice applications',
     author = 'Alec Aivazis',
     author_email = 'alec@aivazis.com',
     url = 'https://github.com/aaivazis/nautilus',
-    download_url = 'https://github.com/aaivazis/nautilus/tarball/0.1.0',
+    download_url = 'https://github.com/aaivazis/nautilus/tarball/0.2.4',
     keywords = ['microservice', 'flask', 'graphql'],
     classifiers = [],
-    test_suite='nose2.collector.collector',
+    test_suite = 'nose2.collector.collector',
+    packages = find_packages(exclude=['example', 'tests']),
     install_requires = [
         'bcrypt',
         'flask',
@@ -27,5 +27,8 @@ setup(
         'graphene',
         'sqlalchemy',
         'nose2',
+        'pika',
+        'python-consul',
+        'singledispatch',
     ]
 )
