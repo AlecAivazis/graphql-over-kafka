586d104c4344351160fe402064bd344a4982fc74
alec aivazis <alec@aivazis.com>
2016-02-11 02:58:01 -0800

    bumped version number

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 
 setup(
     name = 'nautilus',
-    version = '0.2.4',
+    version = '0.2.5',
     description = 'A library for creating microservice applications',
     author = 'Alec Aivazis',
     author_email = 'alec@aivazis.com',
