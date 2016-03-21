eb511e6e7711b0c3e4bc6de3d051567dec551675
Alec Aivazis <alec@aivazis.com>
2016-03-20 19:46:31 -0700

    added tests for base model

--- nautilus/models/tests/test_baseModel.py
+++ nautilus/models/tests/test_baseModel.py
@@ -1,26 +1,57 @@
 import unittest
 from unittest.mock import MagicMock
 
+import nautilus.models as models
+
+
 class TestUtil(unittest.TestCase):
+    """
+        This test suite checks for the various bits of introspective
+        functinality supported by the base model.
+    """
+
+    def setUp(self):
+
+        class User(models.BaseModel):
+            name = models.fields.CharField(null=False, unique=True)
+            date = models.fields.CharField(null=False)
+
+        self.Model = User
+
+
+    def test_can_retrieve_columns(self):
+        assert self.Model.columns == ['name', 'date'], (
+            'Model could not retrieve columns'
+        )
+
+
+    def test_can_retrieve_primary_keys(self):
+        assert self.Model.primary_keys == ['name'], (
+            'Model could not return primary keys'
+        )
+
+
+    def test_can_retrieve_requried_fields(self):
+        assert self.Model.required_fields == ['date'], (
+            'Model could not retrieve required fields.'
+        )
+
 
     def test_can_be_serialized_using_model_encoder(self):
-
         # import the model serializer
-        from sqlalchemy import Column, Text
-        from nautilus.models import ModelSerializer, BaseModel
+        from nautilus.models import ModelSerializer
         import json
 
-        class Model(BaseModel):
-            name =  Column(Text, primary_key = True)
 
-        model = Model(name="foo")
+        model = self.Model(name="foo", date="bar")
 
         serialized = ModelSerializer().serialize(model)
 
-        assert serialized == json.dumps({"name": "foo"}), (
+        assert serialized == json.dumps({
+            "name": "foo", "date": "bar"
+        }), (
             'Model was not correctly serialized'
         )
-
 
 
 
