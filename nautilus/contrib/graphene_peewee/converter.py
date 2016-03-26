2db20885ce8672211b9c471cd6a73e18b9c7aa34
Alec Aivazis <alec@aivazis.com>
2016-03-25 17:46:16 -0700

    cleaned up graphene/peewee converter

--- nautilus/contrib/graphene_peewee/converter.py
+++ nautilus/contrib/graphene_peewee/converter.py
@@ -15,53 +15,46 @@ from graphene.core.types.scalars import (
 # local imports
 import nautilus.models.fields as fields
 
-def convert_peewee_field(field):
-    """
-        This utility converts the given peewee field into the corresponding
-        graphql scalar
-    """
-    return convert_peewee_type(field, field)
-
 @singledispatch
-def convert_peewee_type(field_type, field):
+def convert_peewee_field(field):
     """
         This helper converts a peewee field type into the appropriate type
         for a graphql schema.
     """
     raise ValueError(
-        "Unable to convert peewee field %s (%s)" % (field, field_type)
+        "Unable to convert peewee field %s " % field
     )
 
 
-@convert_peewee_type.register(fields.CharField)
-@convert_peewee_type.register(fields.DateField)
-@convert_peewee_type.register(fields.DateTimeField)
-@convert_peewee_type.register(fields.FixedCharField)
-@convert_peewee_type.register(fields.TextField)
-@convert_peewee_type.register(fields.TimeField)
-@convert_peewee_type.register(fields.UUIDField)
-def convert_field_to_string(field_type, field):
+@convert_peewee_field.register(fields.CharField)
+@convert_peewee_field.register(fields.DateField)
+@convert_peewee_field.register(fields.DateTimeField)
+@convert_peewee_field.register(fields.FixedCharField)
+@convert_peewee_field.register(fields.TextField)
+@convert_peewee_field.register(fields.TimeField)
+@convert_peewee_field.register(fields.UUIDField)
+def convert_field_to_string(field):
     return String(description=field.help_text)
 
 
-@convert_peewee_type.register(fields.PrimaryKeyField)
-def convert_field_to_pk(field_type, field):
+@convert_peewee_field.register(fields.PrimaryKeyField)
+def convert_field_to_pk(field):
     return ID(description=field.help_text)
 
 
-@convert_peewee_type.register(fields.IntegerField)
-@convert_peewee_type.register(fields.BigIntegerField)
-def convert_field_to_int(field_type, field):
+@convert_peewee_field.register(fields.IntegerField)
+@convert_peewee_field.register(fields.BigIntegerField)
+def convert_field_to_int(field):
     return Int(description=field.help_text)
 
 
-@convert_peewee_type.register(fields.DecimalField)
-@convert_peewee_type.register(fields.DoubleField)
-@convert_peewee_type.register(fields.FloatField)
-def convert_field_to_float(field_type, field):
+@convert_peewee_field.register(fields.DecimalField)
+@convert_peewee_field.register(fields.DoubleField)
+@convert_peewee_field.register(fields.FloatField)
+def convert_field_to_float(field):
     return Float(description=field.help_text)
 
 
-@convert_peewee_type.register(fields.BooleanField)
-def convert_field_to_bool(field_type, field):
+@convert_peewee_field.register(fields.BooleanField)
+def convert_field_to_bool(field):
     return Boolean(description=field.help_text)
