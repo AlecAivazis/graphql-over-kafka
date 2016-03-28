908068abbf5119517c784d16b4315436fda14b7d
Alec Aivazis <alec@aivazis.com>
2016-03-28 10:03:37 -0700

    implemented service manager  command in a non-flask context

--- nautilus/services/serviceManager.py
+++ nautilus/services/serviceManager.py
@@ -4,50 +4,58 @@
 """
 
 # external imports
-from flask.ext.script import Manager
-import threading
+import click
 
 class ServiceManager:
 
     def __init__(self, service):
-        # create and attach a command manager for the service
-        self.commandManager = Manager(service.app)
         self.service = service
 
-        @self.commandManager.command
-        def syncdb():
-            """ Create the database entries. """
-            # import the db module
-            from nautilus.db import db
-            # create all of the tables
-            db.create_all()
-            # notify the user
-            print("Successfully created database entries.")
+        @click.group()
+        def group():
+            pass
 
-        @self.commandManager.command
-        def runserver(
-            host = '127.0.0.1',
-            port = 8000,
-            debug = False,
-            secretKey = 'supersecret'
-        ):
-            """ Start the service. """
+        @group.command()
+        def syncdb():
+            print('sync db')
+
+        @group.command()
+        @click.option('--port', default=8000, help="The port for the service http server.")
+        @click.option('--host', default='127.0.0.1', help="The host for the http server.")
+        def runserver(port, host):
+            # run the service
             service.run(
                 host = host,
                 port = int(port),
-                debug = debug,
-                secretKey = secretKey
             )
+        #
+        # @self.commandManager.command
+        # def syncdb():
+        #     """ Create the database entries. """
+        #     # import the db module
+        #     from nautilus.db import db
+        #     # create all of the tables
+        #     db.create_all()
+        #     # notify the user
+        #     print("Successfully created database entries.")
+
+        # save the command group to the manager
+        self.group = group
 
 
     def run(self):
         """ run the command manager """
         try:
-            self.commandManager.run()
+            # run the command group
+            self.group()
+        # if the user interrupts the execution
         except KeyboardInterrupt:
             print()
             print("Cleaning up service...")
+            # stop the service
             self.service.stop()
+        # if there is a normal exception
         except Exception as err:
             print("Closing due to error: %s" % err)
+            # stop the service and clean up
             self.service.stop()
