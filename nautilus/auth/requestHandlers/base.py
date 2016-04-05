03897128755e9bf9df626266ab5afcada8e94a14
Alec Aivazis <alec@aivazis.com>
2016-04-04 19:27:20 -0700

    configured AuthRequestHandler to redirect to auth service for login

--- nautilus/auth/requestHandlers/base.py
+++ nautilus/auth/requestHandlers/base.py
@@ -1,6 +1,8 @@
 # local imports
 from nautilus.network.http import RequestHandler
 from nautilus.network import query_service
+from nautilus.conventions.services import auth_service_name
+from nautilus.network.registry import service_location_by_name
 
 class AuthRequestHandler(RequestHandler):
     # NEED SOME FORM OF:
@@ -14,17 +16,27 @@ class AuthRequestHandler(RequestHandler):
         return self.get_secure_cookie(self._token_name)
 
 
+    def get_login_url(self):
+        """
+            This method returns the url that handles logging-in.
+        """
+        # return the location of the service with the conventional auth name
+        return "http://" + service_location_by_name(auth_service_name())
+
+
     def login_user(self, user):
         """
             This function logs the given user in on the request request.
         """
         self.set_secure_cookie(self._token_name, self._token_contents(user))
 
+
     def logout_user(self):
         """
             This method logs the current user out by clearing the cookie.
         """
         self.clear_cookie(self._token_name)
+
 
     @property
     def _token_name(self):
