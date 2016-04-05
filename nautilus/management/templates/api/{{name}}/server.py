0f501660cb6ee86657381daba09f3165330c3939
Alec Aivazis <alec@aivazis.com>
2016-04-05 14:28:20 -0700

    updated service templates

--- nautilus/management/templates/api/{{name}}/server.py
+++ nautilus/management/templates/api/{{name}}/server.py
@@ -3,5 +3,5 @@ from nautilus import APIGateway
 # local imports
 from .schema import schema
 
-# create a nautilus service with just the schema
-service = APIGateway(schema=schema)
+class {{name.title()}}(nautilus.APIGateway):
+    schema = schema
--- nautilus/management/templates/model/{{name}}/server.py
+++ nautilus/management/templates/model/{{name}}/server.py
@@ -4,15 +4,14 @@ from nautilus import ModelService
 from sqlalchemy import Column, Text
 from nautilus.models import HasID, BaseModel, CRUDNotificationCreator
 
-class {{name.title()}}(CRUDNotificationCreator, HasID, BaseModel):
+class {{name.title()}}(CRUDNotificationCreator, BaseModel):
     pass
 
 
 class ServiceConfig:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/{{name}}.db'
+    databuse_url = 'sqlite:////tmp/{{name}}.db'
 
 
-service = ModelService(
-    configObject = ServiceConfig,
-    model = {{name.title()}},
-)
+class {{name.title()}}(nautilus.ModelService):
+    model = {{name.title()}}
+    config = ServiceConfig
