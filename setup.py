4a8f0424994d451f3beb989122fd043e99891136
alec aivazis <alec@aivazis.com>
2016-04-24 12:05:54 -0700

    package version and download link are now in sync

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='nautilus',
-    version='0.4.6',
+    version='0.4.7',
     description='A library for creating microservice applications',
     author='Alec Aivazis',
     author_email='alec@aivazis.com',
     url='https://github.com/AlecAivazis/nautilus',
-    download_url='https://github.com/aaivazis/nautilus/tarball/0.4.5',
+    download_url='https://github.com/aaivazis/nautilus/tarball/0.4.7',
     keywords=['microservice', 'tornado', 'graphql'],
     test_suite='nose2.collector.collector',
     packages=find_packages(exclude=['example', 'tests']),
