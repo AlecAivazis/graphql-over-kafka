b006cb8329074d7b1653bbbbd1ae904693c99856
Alec Aivazis <alec@aivazis.com>
2016-06-18 03:38:59 -0700

    added decorator for async tests

---
+++ tests/util/decorators.py
@@ -0,0 +1,25 @@
+# external imports
+import asyncio
+from unittest.mock import MagicMock
+# local imports
+from nautilus.network.events.consumers import KafkaBroker
+
+loop = asyncio.get_event_loop()
+
+def async_test(test_function):
+    """
+        This decorator wraps a test function and executes it on the asyncio
+        event loop.
+    """
+
+    # test_function()
+
+    def function(*args, **kwds):
+
+        # execute the test on the event loop
+        handler = loop.run_until_complete(test_function(*args, **kwds))
+        print(handler)
+        # close the event loop
+        loop.close()
+
+    return function
