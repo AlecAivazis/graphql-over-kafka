0ab7c053a4009de46a67effc390208d5ef998c8f
Alec Aivazis <alec@aivazis.com>
2016-08-16 04:24:02 -0700

    implemented publish cli using ActionHandler class

--- nautilus/management/scripts/publish.py
+++ nautilus/management/scripts/publish.py
@@ -6,8 +6,8 @@
 # external imports
 import click
 import asyncio
-import json
-from aiokafka import AIOKafkaProducer
+# local imports
+from nautilus.network.events.consumers import ActionHandler
 
 @click.option('--type', '-t', default='cli', help="The action type of the action to publish.")
 @click.option('--payload', '-p', required=True, help="The payload of the message")
@@ -17,31 +17,22 @@ def publish(type, payload):
         Publish a message with the specified action_type and payload over the
         event system. Useful for debugging.
     """
-    async def _produce(loop):
-        # the message payload
-        message = json.dumps({
-            'action_type': type,
-            'payload': payload
-        }).encode()
+    async def _produce():
+        # fire an action with the given values
+        await producer.send(action_type=type, payload=payload)
+        # notify the user that we were successful
+        print("Successfully dispatched action with type {}.".format(type))
 
-        # adds message to sending queue
-        future = await producer.send('actions', message)
-        # waiting for message to be delivered
-        r = await future
-
-        # notify the user
-        print("Successfully published action to services.")
-
-    # grab a reference to the current event loop
-    loop = asyncio.get_event_loop()
     # create a producer
-    producer = AIOKafkaProducer(loop=loop, bootstrap_servers='localhost:9092')
+    producer = ActionHandler()
+    # start the producer
+    producer.start()
+
+    # get the current event loop
+    loop = asyncio.get_event_loop()
+
     # run the production sequence
-    loop.run_until_complete(producer.start())
-    loop.run_until_complete(_produce(loop))
-    loop.run_until_complete(producer.stop())
-    # close the event loop
-    loop.close()
+    loop.run_until_complete(_produce())
 
-
-
+    # start the producer
+    producer.stop()
